3a7ac1e0bb62e7c4e98e6e3843477d7d1464c805
github username <github email address>
2019-12-10 17:34:29 -0500

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,10 @@
 class Dog
 
-    def 
-end
-lassie = Dog.new
-lassie.name ="Lassie"
+    def name=(dogs_name)
+      @this_dogs_name = dogs_name
+    end
+  #   return the instance of the 
+    def name
+      @this_dogs_name
+    end
+  end
